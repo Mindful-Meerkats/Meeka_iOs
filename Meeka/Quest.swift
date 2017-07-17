6ae06b8aa9761d9bf77aee2ece66f5087c66c889
jjwedemyer <mail@jakob-wedemyer.de>
2017-07-17 11:55:40 +0200

    populated init(), created init?() with the decoder for the props, encode() populated, renamed description to desc

--- Meeka/Quest.swift
+++ Meeka/Quest.swift
@@ -8,27 +8,54 @@
 
 import Foundation
 import CoreData
+import os.log
 
 class Quest:NSObject,NSCoding {
     //Properties
     var status:         State
     var id:             Int
     var title:          String
-    var description:    String
+    var desc:           String
     var completedText:  String
     var points:         MMQPoints
-    var penalty?:       MMQPoints
+    var penalty:        MMQPoints? //don't really know if used
     
     
     //Init section
-    init() {
+    init(id: Int, status:State ,title: String, desc: String, completedText: String, points: MMQPoints, penalty: MMQPoints?) {
+        self.id             = id
+        self.status         = status
+        self.title          = title
+        self.desc           = desc
+        self.completedText  = completedText
+        self.points         = points
+        self.penalty        = penalty
+        
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let id = aDecoder.decodeObject(forKey: QuestPropertyKey.id) as? Int else { os_log("Could not decode ID") }
+        guard let status = aDecoder.decodeObject(forKey: QuestPropertyKey.status) as? State  else { os_log("Could not decode Status") }
+        guard let title = aDecoder.decodeObject(forKey: QuestPropertyKey.title) as? String else { os_log("Could not decode Title") }
+        guard let desc = aDecoder.decodeObject(forKey: QuestPropertyKey.desc) as? String else { os_log("Could not decode Desc") }
+        guard let completedText = aDecoder.decodeObject(forKey: QuestPropertyKey.completedText) as? String else { os_log("Could not decode Completed Text") }
+        guard let points = aDecoder.decodeObject(forKey: QuestPropertyKey.points) as? MMQPoints else { os_log("Could not decode Points") }
+        let penalty = aDecoder.decodeObject(forKey: QuestPropertyKey.penalty) as? MMQPoints
+        
+        self.init(id:id, status:status,desc:desc,completedText:completedText,points:points, penalty:penalty)
         
     }
     
     //NSCoding
     func encode(with aCoder: NSCoder) {
         aCoder.encode(id,forKey:QuestPropertyKey.id)
-        aCoder.encode(status, forKey: Quest)
+        aCoder.encode(status, forKey: QuestPropertyKey.status)
+        aCoder.encode(title, forKey: QuestPropertyKey.title)
+        aCoder.encode(desc, forKey: QuestPropertyKey.desc)
+        aCoder.encode(completedText, forKey: QuestPropertyKey.completedText)
+        aCoder.encode(points, forKey: QuestPropertyKey.points)
+        aCoder.encode(penalty, forKey: QuestPropertyKey.penalty)
+        
     }
     
     // Subclasses
@@ -44,7 +71,7 @@ class Quest:NSObject,NSCoding {
         var soul:       Int
         
         //init section
-        init?(coder aDecoder: NSCoder) {
+        required init?(coder aDecoder: NSCoder) {
             
         }
         
@@ -71,7 +98,7 @@ struct QuestPropertyKey{
     static let status        = "status"
     static let id            = "id"
     static let title         = "title"
-    static let description   = "desc"
+    static let desc          = "desc"
     static let completedText = "comText"
     static let points        = "points"
     static let penalty       = "penalty"
